56846082850a4a3682ae6806670a8eb6d7a3bd41
elescars <scarsellielena@gmail.com>
2020-05-16 15:42:46 +0200

    Receiver e CountDownTimer

--- app/src/main/java/it/unipi/covid5b/MainActivity.java
+++ app/src/main/java/it/unipi/covid5b/MainActivity.java
@@ -2,14 +2,16 @@ package it.unipi.covid5b;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.BroadcastReceiver;
+//import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentSender;
 import android.os.Bundle;
-import android.os.CountDownTimer;
+//import android.os.CountDownTimer;
 import android.util.Log;
 import java.util.Timer;
+import it.unipi.covid5b.BroadcastReceiver;
+import it.unipi.covid5b.CountDownTimer;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -19,28 +21,13 @@ public class MainActivity extends AppCompatActivity {
         //new MyReceiver();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Timer initialization
-        new CountDownTimer(9000000, 1000) { //timer di 15 minuti, tick di 1 secondo
-            @Override
-            public void onTick(long millisUntilFinished) {
-                //check classificatore
-            }
-            public void onFinish() {
-                Log.d("MY_WASH", "Non ti sei lavato le mani");
-                /*
-                Se arrivo qui, scade il timer, non è stato effettuato il lavaggio delle mani
-                Quindi mando l'intent broadcast
-                MainActivity.send();
-                */
-                send();
-            }
+        new BroadcastReceiver.BroadcastReceiver.MyBroadcastReceiver(getApplicationContext(), /*intent ricevuto*/);
+        //Timer initialization: 15 minutes, tick every 1 second
+        new CountDownTimer(9000000, 1000) {
         }.start();
     }
-    public void send() {
-        Intent intent=new Intent(getApplicationContext(), /*risultato del classificatore*/);
-        intent.setFlags(0);
-        sendBroadcast(intent);
-    }
+
+
 }
 
 
