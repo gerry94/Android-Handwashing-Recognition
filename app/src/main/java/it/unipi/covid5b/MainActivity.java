bf36944b1692594d0abc779fa758193803339c17
gerry94 <g.alvaro@studenti.unipi.it>
2020-05-18 14:37:10 +0200

    Broadcast

--- app/src/main/java/it/unipi/covid5b/MainActivity.java
+++ app/src/main/java/it/unipi/covid5b/MainActivity.java
@@ -1,33 +1,31 @@
 package it.unipi.covid5b;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-//import android.content.BroadcastReceiver;
-import android.content.Context;
 import android.content.Intent;
-import android.content.IntentSender;
+import android.content.IntentFilter;
 import android.os.Bundle;
-//import android.os.CountDownTimer;
 import android.util.Log;
-import java.util.Timer;
-import it.unipi.covid5b.BroadcastReceiver;
-import it.unipi.covid5b.CountDownTimer;
-
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    ConnectionReceiver receiver;
+    IntentFilter intentFilter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        //Ricezione dell'intent
-        //new MyReceiver();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        new BroadcastReceiver.BroadcastReceiver.MyBroadcastReceiver(getApplicationContext(), /*intent ricevuto*/);
-        //Timer initialization: 15 minutes, tick every 1 second
-        new CountDownTimer(9000000, 1000) {
-        }.start();
+
+        receiver = new ConnectionReceiver();
+        intentFilter = new IntentFilter("SOME_ACTION");
     }
 
-
+    public void sendMessage(View view) {
+        Intent intent = new Intent("SOME_ACTION");
+        sendBroadcast(intent);
+        Log.d("MSG","Intent sent in broadcast");
+    }
 }
 
 
