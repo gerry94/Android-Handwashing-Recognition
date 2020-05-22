66031e0a2f1706b8c92bdc5861229e7c231a7c40
riccardo <riccardo94p@hotmail.com>
2020-05-22 15:17:53 +0200

    merge intent + classificatori

--- MyApplication/app/src/main/java/com/example/myapplication/ConnectionReceiver.java
+++ MyApplication/app/src/main/java/com/example/myapplication/ConnectionReceiver.java
@@ -4,18 +4,45 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.location.LocationManager;
-import android.os.CountDownTimer;
 import android.util.Log;
 import android.widget.Toast;
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class ConnectionReceiver extends BroadcastReceiver {
 
     String key = null;
     Boolean entering = true;
+
+    final int MIN_ACTIVITY_TIME = 10; //time that the user SHOULD take to perform the desired activity (in seconds)
+
+    //contains the results of the classifier during a time window in which the user might be washing hands
+    List<Double> accelVector = new ArrayList<>(Collections.nCopies(MIN_ACTIVITY_TIME, 0.0));
+    int accelInserts = 0;
+
+    List<Double> gyroVector = new ArrayList<>(Collections.nCopies(MIN_ACTIVITY_TIME, 0.0));
+    int gyroInserts = 0;
+
+    //provare come cambia il classificatore al variare di THRESHOLD e OVERLAP
+    final double THRESHOLD = 0.7*MIN_ACTIVITY_TIME; //70%
+    final double OVERLAP = 0.5; //50% overlapped windows
+
+    final double ACC_WEIGHT = 0.96;
+    final double GYR_WEIGHT = 0.8;
+    final int WINDOW_SIZE = 50;
+
+    MotionSensor accelData = new MotionSensor();
+    MotionSensor gyrData = new MotionSensor();
+
+    public void sendIntent(Context context) {
+        Intent intentWashing = new Intent("handWashed");
+        context.sendBroadcast(intentWashing);
+        Log.d("MSG","Intent sent in broadcast");
+    }
 
     @Override
     public void onReceive(Context context, Intent intent) {
@@ -23,7 +50,6 @@ public class ConnectionReceiver extends BroadcastReceiver {
         Log.d("MSG","Ricevuto intent..."+intent.getAction());
 
         if(intent.getAction().equals("home_proximity")) {
-
             Toast.makeText(context, "home_proximity is received", Toast.LENGTH_LONG).show();
 
             //controllare se ritorno a casa o se sta andando via con campo KEY_PROXIMITY_ENTERING (è lo stesso intent)
@@ -32,46 +58,89 @@ public class ConnectionReceiver extends BroadcastReceiver {
 
             //se rientro a casa
             if(entering) {
-                //Se utente tornato a casa, carico file csv, starto il timer e procedo con la classificazione
-                /*BufferedReader reader = null;
                 try {
-                    reader = new BufferedReader(new FileReader("c://prova.txt"));
-                } catch (FileNotFoundException e) {
-                    Log.d("ERROR", "Errore lettura file");
-                    e.printStackTrace();
-                }
-                String line = null;
-                try {
-                    line = reader.readLine();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                while (line != null) {
-                    System.out.println(line);
-                    try {
-                        line = reader.readLine();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-                */
-                //Timer 5 secondi, tick ogni secondo
-                new CountDownTimer(5000, 1000) {
-                    public void onTick(long millisUntilFinished) {
-                        //Check classifier
-                        Log.d("TIMER", "Tick");
-                    }
+                    //read csv file (first 15mins) and classify
+                    BufferedReader br = new BufferedReader(new FileReader("data/accelData.csv"));
+                    BufferedReader br2 = new BufferedReader(new FileReader("data/gyrData.csv"));
+                    String separator = ",", line = br.readLine(), line2 = br2.readLine();
 
-                    public void onFinish() {
-                        //Timer expired
-                        Log.d("TIMER", "End");
+                    int linesRead = 0;
+                    //90000 lines = 15mins worth of data for both sensors (45000 lines each)
+                    while( (linesRead < 90000) && line != null && line2 != null)
+                    {
+                        String[] coord = line.split(separator);
+                        accelData.add(Double.parseDouble(coord[0]), Double.parseDouble(coord[1]), Double.parseDouble(coord[2]));
+
+                        coord = line2.split(separator);
+                        gyrData.add(Double.parseDouble(coord[0]), Double.parseDouble(coord[1]), Double.parseDouble(coord[2]));
+
+                        if ( accelData.getNumData() == WINDOW_SIZE)
+                        {
+                            accelData.computeFeatures();
+                            try {
+                                Double result = WekaAccelClassifier.classify(accelData.getFeatures());
+
+                                //inserting at the end, popping the first element
+                                accelVector.add(result);
+                                accelVector.remove(0);
+                                accelInserts++;
+
+                                if(accelInserts == OVERLAP*MIN_ACTIVITY_TIME) {
+                                    double s1 = 0, s2 = 0;
+                                    for(double num: accelVector)
+                                        s1 = s1+num;
+                                    for(double num: gyroVector)
+                                        s2 = s2+num;
+
+                                    if( ( (s1*ACC_WEIGHT)+(s2*GYR_WEIGHT) )/2 >= THRESHOLD ) {
+                                        //send intent
+                                        sendIntent(context);
+                                        break;
+                                    }
+                                    accelInserts = 0;
+                                }
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
+                            accelData.flush();
+                        }
+                        if(gyrData.getNumData() == WINDOW_SIZE) {
+                            gyrData.computeFeatures();
+                            try {
+                                Double result = WekaGyroClassifier.classify(gyrData.getFeatures());
+
+                                //inserting at the end, popping the first element
+                                gyroVector.add(result);
+                                gyroVector.remove(0);
+                                gyroInserts++;;
+
+                                if(gyroInserts == OVERLAP*MIN_ACTIVITY_TIME) {
+                                    double s1 = 0, s2 = 0;
+                                    for(double num: accelVector)
+                                        s1 = s1+num;
+                                    for(double num: gyroVector)
+                                        s2 = s2+num;
+
+                                    if( ( (s1*ACC_WEIGHT)+(s2*GYR_WEIGHT) )/2 >= THRESHOLD ) {
+                                        //send intent
+                                        sendIntent(context);
+                                        break;
+                                    }
+                                    gyroInserts = 0;
+                                }
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
+                            gyrData.flush();
+                        }
+                        line = br.readLine();
+                        line2 = br2.readLine();
                     }
-                }.start();
-            }else{
+                } catch(IOException e) { e.printStackTrace(); }
+            } else {
                 //se invece sta uscendo da casa, non faccio nulla e mi rimetto in ricezione
                 Toast.makeText(context, "Utente uscito da casa", Toast.LENGTH_LONG).show();
             }
-
         }
         else {
             //Ricevuto intent non di interesse
--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,70 +1,16 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.content.Intent;
-import android.content.IntentFilter;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     ConnectionReceiver receiver;
-    IntentFilter intentFilter;
-    WekaGyroClassifier gClassifier = null;
-    WekaAccelClassifier aClassifier = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        aClassifier = new WekaAccelClassifier();
-        double result = 1234.0;
-        // Array di features, 3 (XYZ) x 7 features estratte (mean, var....) = 21
-        /*
-        'mean_x'
-        'max_x'
-        'min_x'
-        'std_x'
-        'var_x'
-        'ske_x'
-        'zcr_x'
-        'mean_y'
-        'max_y'
-        'min_y'
-        'std_y'
-        'var_y'
-        'ske_y'
-        'zcr_y'
-        'mean_z'
-        'max_z'
-        'min_z'
-        'std_z'
-        'var_z'
-        'ske_z'
-        'zcr_z'
-        'handwash'
-
-         */
-        Double arr[] = new Double[] {1.2, 5.6, 3.4, 2.9, 9.7, 1.2, 5.6, 3.4, 2.9, 9.7, 1.2, 5.6, 3.4, 2.9, 9.7, 1.2, 5.6, 3.4, 2.9, 9.7,2.3 };
-        try {
-            result = aClassifier.classify(arr);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        Log.d("Class", Double.toString(result) );
-
-
         receiver = new ConnectionReceiver();
-        //intentFilter = new IntentFilter("SOME_ACTION");
-    }
-
-    //L'invio dell'intent in realtà sarà quando viene riconosciuto lavaggio mani
-    public void sendMessage(View view) {
-        Intent intentWashing = new Intent("handWashed");
-        sendBroadcast(intentWashing);
-        Log.d("MSG","Intent sent in broadcast");
     }
 }
